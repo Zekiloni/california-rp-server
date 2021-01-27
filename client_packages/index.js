5426bc289da217124aceb80607ff4b9480520e86
Zekirija <zekirija2001@hotmail.com>
2021-01-27 18:18:03 +0100

    interactions menu half finished

--- client_packages/index.js
+++ client_packages/index.js
@@ -5,7 +5,8 @@ var animals = require('./animals')
 var charClohing = require('./clothing/clothing')
 var charCustomization = require('./char-customization/customization')
 var playerDamage = require('./playerDamage')
-var customChat =require('./chat/index')
+var customChat = require('./chat/index')
+var interactionsMenu = require('./interactions-menu/interactions')
 
 
 
---
+++ client_packages/interactions-menu/interactions.js
@@ -0,0 +1,26 @@
+
+var player =  mp.players.local, interactionsCEF, opened = false;
+
+mp.events.add({
+   'client:showInteractions': () => {
+      interactionsCEF = mp.browsers.new('package://interactions-menu/interactions-interface/interact.html');
+      setTimeout(() => { mp.gui.cursor.show(true, true); }, 500);
+   },
+
+   'client:hideInteractions': () => {
+      interactionsCEF.destroy();
+      setTimeout(() => { mp.gui.cursor.show(false, false); }, 600);
+   },
+})
+
+
+mp.keys.bind(0x4D, false, function() {
+   if(opened == false) {
+      opened = true;
+      mp.events.call('client:showInteractions');
+   }
+   else { 
+      opened = false;
+      mp.events.call('client:hideInteractions');
+   }
+});
