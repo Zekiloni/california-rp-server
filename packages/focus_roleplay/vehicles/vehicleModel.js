95d04d72b308a1674569bbce4e662d7f00411cfa
Zekirija <zekirija2001@hotmail.com>
2021-01-28 00:10:52 +0100

    vehiclemodel

--- packages/focus_roleplay/vehicles/vehicleModel.js
+++ packages/focus_roleplay/vehicles/vehicleModel.js
@@ -17,42 +17,6 @@ module.exports = class VehicleModel {
        this.suspension = suspension || -1;
        allVehicles.push(this);
    }
-   
-   // get id() { return this.id; } 
-   // set id(x) { this.id = x; }
-
-   // get model() { return this.locked; } 
-   // set model(x) { this.model = x; }
-
-   // get locked() { return this.locked; } 
-   // set locked(x) { this.locked = x; }
-
-   // get owner() { return this.owner; }
-   // set owner(x) { this.owner = x; }
-
-   // get price() { return this.price; }
-   // set price(x) { this.price = x; }
-
-   // get position() { return this.position; }
-   // set position(x) { this.position = x; }
-
-   // get fuel() { return this.fuel; }
-   // set fuel(x) { this.fuel = x; }
-
-   // get color() { return this.color; };
-   // set color(x) { this.color = [x.rgb, x.rgb2]; }
-
-   // get mods() { return this.mods; }
-   // set mods(x) { this.mods = x; }
-
-   // get transmission() { return this.transmission; }
-   // set transmission(x) { this.transmission = x; }
-
-   // get engine() { return this.engine; }
-   // set engine(x) { this.engine = x; }
-
-   // get brakes() { return this.brakes; }
-   // set brakes(x) { this.brakes = x; }
 
    info () { return this; }
 }
