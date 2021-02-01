221ef72ffa65e2b4344099ff6e7b565df5548051
Zekirija <zekirija2001@hotmail.com>
2021-02-01 18:43:54 +0100

    cuff uncuff

--- packages/focus_roleplay/factions/factionCmds.js
+++ packages/focus_roleplay/factions/factionCmds.js
@@ -1,10 +1,8 @@
 mp.events.addCommand({
   
    'duty': (player, fullText) => {
-
-      if (player.faction == 0) return player.outputChatBox(`Niste ni u jednoj fakciji !`);
+      if (!fac.isPlayerFactionType(player, FACTIONS_TYPES.LAW)) return player.outputChatBox(`Vasa fakcija nije odgovarajuceg tipa !`);
       let faction = fac.getFaction(player.faction);
-      if (!faction.TYPE == FACTIONS_TYPES.LAW) return player.outputChatBox(`Vasa fakcija nije odgovarajuceg tipa !`);
       let dPos = faction.EQUIP_POINT;
       let dutyPos = new mp.Vector3(dPos.x, dPos.y, dPos.z)
       if (player.dist(dutyPos) > 2) return player.outputChatBox(`Ne nalazite se u blizini ormarica !`);
@@ -26,18 +24,31 @@ mp.events.addCommand({
       //radio.send(player, 911)
    },
 
-   'cuff': (player, fullText, taget) => { 
-      if(!fac.isPlayerFactionType(player, FACTIONS_TYPES.LAW)) return player.outputChatBox(`Vasa fakcija nije odgovarajuceg tipa !`);
+   'cuff': (player, fullText, target) => { 
+      if (!fac.isPlayerFactionType(player, FACTIONS_TYPES.LAW)) return player.outputChatBox(`Vasa fakcija nije odgovarajuceg tipa !`);
+      if (!target) return player.outputChatBox(`Koriscenje /cuff [igrac] !`);
       let recipient = account.findPlayer(target);
+      let meText;
 
       if(!recipient) { 
          player.outputChatBox('Korisnik nije pronadjen'); 
          return false; 
       } 
 
-      recipient.removeAllWeapons();
-      recipient.outputChatBox(`${player.name} vam je oduzeo vasa oruzija.`);
-      player.outputChatBox(`Oduzeli ste sva oruzija ${recipient.name}.`);
+      if(!recipient.cuffed) { 
+         recipient.call('client:playerCuff');
+         player.cuffed = true;
+         meText = `uzima par ZIP lisica sa pojasa te ih stavlja ${recipient.name}`;
+         recipient.playAnimation('mp_arresting', 'idle', 1, 49);
+      }
+      else { 
+         recipient.call('client:playerUncuff');
+         player.cuffed = false;
+         meText = `je skinuo lisice ${recipient.name}`;
+         recipient.stopAnimation();
+      }
+
+      account.sendProxMessage(player, CHAT_RADIUS.ME, `* ${player.name} ${meText}.`, 'F9B7FF', 'E6A9EC', 'C38EC7', 'D2B9D3');
    },
 
    'drag': (player, fullText, target) => { 
