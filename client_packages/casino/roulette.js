6f3634e08d91a0620ec16ab1e9e914bd1b4084d6
Marko Koprivanac <marko.koprivanac.19@singimail.rs>
2021-03-09 20:37:46 +0100

    rulet

--- client_packages/casino/roulette.js
+++ client_packages/casino/roulette.js
@@ -179,3 +179,36 @@ mp.events.add('playerExitColshape', (shape) => {
 		casinoSeatToJoin = null;
 	}
 });
+
+getCameraHitCoord = () =>
+{
+	let position = rouletteCamera.getCoord();
+	let direction = rouletteCamera.getDirection();
+	let farAway = new mp.Vector3((direction.x * 150) + position.x, (direction.y * 150) + position.y, (direction.z * 150) + position.z);
+	
+	let hitData = mp.raycasting.testPointToPoint(position, farAway, mp.players.local);
+	
+	if(hitData != undefined)
+	{
+		return hitData;
+	}
+	return null;
+}
+
+getNormalizedVector = (vector) =>
+{
+	let mag = Math.sqrt(vector.x * vector.x + vector.y * vector.y + vector.z * vector.z);
+	vector.x = vector.x / mag;
+	vector.y = vector.y / mag;
+	vector.z = vector.z / mag;
+	return vector;
+}
+
+getCrossProduct = (v1, v2) =>
+{
+	let vector = new mp.Vector3(0, 0, 0);
+	vector.x = v1.y * v2.z - v1.z * v2.y;
+	vector.y = v1.z * v2.x - v1.x * v2.z;
+	vector.z = v1.x * v2.y - v1.y * v2.x;
+	return vector;
+}
