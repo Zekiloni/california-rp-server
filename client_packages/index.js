7a582ba559a6fced923e49b4d712efc72fb2238f
Marko Koprivanac <marko.koprivanac.19@singimail.rs>
2021-03-14 21:34:22 +0100

    afk + objectSync + sortiranje foldera

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,25 +1,25 @@
-
-var gtaUI = require('./playerHud')
-var login = require('./authentication/login')
-var animals = require('./animals')
-var charClohing = require('./clothing/clothing')
-var charCustomization = require('./char-customization/customization')
-var playerDamage = require('./syncDamage')
-var inventory = require('./inventory/inventory')
-var binds = require('./binds')
-var events = require('./events')
+var gtaUI = require('./player/playerHud');
+var login = require('./authentication/login');
+var animals = require('./animals');
+var charClohing = require('./clothing/clothing');
+var charCustomization = require('./char-customization/customization');
+var playerDamage = require('./syncDamage');
+var inventory = require('./inventory/inventory');
+var binds = require('./player/binds');
+var events = require('./events');
 // var interactionsMenu = require('./interactions-menu/interactions')
-var bankingATM = require('./banking/atm')
-var vehicles = require('./vehicles')
-var chatBubble = require('./chatBubble')
-var playerList = require('./player-list/playerList')
-var police = require('./factions/police/lspd')
-var vehDealership = require('./vehicles-dealership/dealerships')
-var jobs = require('./jobs/jobs')
-var cayoPerico = require('./heistisland')
-var nameTags = require('./nametags')
-var chat = require('./chat')
-var furniture = require('./furniture')
+var bankingATM = require('./banking/atm');
+var vehicles = require('./vehicles');
+var chatBubble = require('./player/chatBubble');
+var playerList = require('./player-list/playerList');
+var police = require('./factions/police/lspd');
+var vehDealership = require('./vehicles-dealership/dealerships');
+var jobs = require('./jobs/jobs');
+var cayoPerico = require('./heistisland');
+var nameTags = require('./player/nametags');
+var chat = require('./chat');
+var furniture = require('./furniture');
+var fingerPointer = require('./player/fingerPointing');
 
 
 
