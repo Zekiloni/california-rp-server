5df8aa07d1f21d7db668bc791a0901c6084d7a7f
Zekirija <zekirija2001@hotmail.com>
2021-04-10 16:38:34 +0200

    a

--- packages/focus_roleplay/classes/commands/admin.commands.js
+++ packages/focus_roleplay/classes/commands/admin.commands.js
@@ -19,6 +19,22 @@ module.exports = {
          }
       },
    
+
+      {
+         name: 'revive',
+         admin: 2,
+         call: (player, args) => { 
+            let target = mp.players.find(args[0]);
+            if (target) { 
+               let position = target.position;
+               clearTimeout(target.respawnTimer)
+               target.isDead = false;
+               setTimeout(() => { target.spawn(position); }, 700)
+
+            }
+         }
+      },
+
       {
          name: 'freeze',
          call: (player, args) => { 
@@ -36,7 +52,7 @@ module.exports = {
          call: (player, args) => {
             if(player.admin < 2) return player.notification(MSG_NOT_ALLOWED, NOTIFY_ERROR, 4);
             let vehToFix = player.vehicle;
-            if(!vehToFix) 
+            if (!vehToFix) 
                return player.notification('Morate biti u vozilu.', NOTIFY_ERROR, 4);
             else
                vehToFix.repair();
