a05c26b9a801715d0f855a6ea86d4f262b01ca3f
Zekirija <zekirija2001@hotmail.com>
2021-04-15 01:02:15 +0200

    a

--- client_packages/player/Interactions/Animations.js
+++ client_packages/player/Interactions/Animations.js
@@ -1,16 +1,34 @@
 
 
 const player = mp.players.local;
+let animation = { 
+	name: null,
+	dictionary: null
+}
 
 mp.events.add({
 	'entityStreamIn': (entity) => {
 	
 	},
 
-	'client:player.animation': (animation, dictionary) => {
-		mp.game.streaming.requestAnimDict(dictionary);
-		while (!mp.game.streaming.hasAnimDictLoaded(dictionary)) mp.game.wait(0);
-		player.taskPlayAnim(dictionary, animation, 8.0, 0.0, -1, 0, 0.0, false, false, false);
-		
+	'client:player.animation': (animName, animDict, flag, time = 5) => {
+		mp.game.streaming.requestAnimDict(animDict);
+		while (!mp.game.streaming.hasAnimDictLoaded(animDict)) mp.game.wait(0);
+		player.taskPlayAnim(animDict, animName, 8.0, 0.0, -1, flag, 0.0, false, false, false);	
+		animation.name = animName;
+		animation.dictionary = animDict;
+		setTimeout(() => {
+			player.stopAnimTask(animDict, animName, 3.0);
+		}, time * 1000);
 	}
 })
+
+mp.keys.bind(0x20, true, function() {
+	if (player.logged && player.spawned) { 
+		let playing = player.isPlayingAnim(animation.dictionary, animation.name, 3);
+		if (playing) { 
+			player.stopAnimTask(animation.dictionary, animation.name, 3.0);
+		}
+	}
+});
+
