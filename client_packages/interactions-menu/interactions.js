ec21a5268dcb79d0516a61a1362c8c085361dc6e
Kopra99 <you@example.com>
2021-04-16 00:41:59 +0200

    interakcije

--- client_packages/interactions-menu/interactions.js
+++ client_packages/interactions-menu/interactions.js
@@ -14,8 +14,7 @@ mp.events.add({
 })
 
 
-mp.keys.bind(0x4D, false, function() {
-   if(opened == false) {
+/*if(opened == false) {
       if (mp.players.local.isTypingInTextChat) return;
       opened = true;
       mp.events.call('client:showInteractions');
@@ -23,5 +22,21 @@ mp.keys.bind(0x4D, false, function() {
    else { 
       opened = false;
       mp.events.call('client:hideInteractions');
-   }
+   } */
+
+mp.keys.bind(0x4D, false, function() {
+   if (mp.players.local.isTypingInTextChat) return;
+   if (!player.data.spawned) return;
+   mp.gui.cursor.visible = !mp.gui.cursor.visible;
+});
+
+mp.events.add('click', (x, y, upOrDown, leftOrRight) => {
+   let pos3d = mp.game.graphics.screen2dToWorld3d([x, y]);
+   const camera = mp.cameras.new("gameplay"); // gets the current gameplay camera
+   let position = camera.getCoord();
+
+   const end = lerp(position, pos3d, 5);
+   const result = mp.raycasting.testPointToPoint(position, end, [mp.players.local]);
+ 
+   console.log(JSON.stringify(result));
 });
