c346b2227ce54ae5ef27d2823e7de6eae564dd89
Zekirija <zekirija2001@hotmail.com>
2021-04-18 23:49:42 +0200

    a

--- packages/focus_roleplay/classes/commands/police.commands.js
+++ packages/focus_roleplay/classes/commands/police.commands.js
@@ -10,17 +10,29 @@ module.exports = {
             }
             
             if (character.gender == 1) { 
-               player.setClothes(11, 143, 0, 0);
-               player.setClothes(8, 122, 0, 0);
-               player.setClothes(4, 277, 0, 0);
-               player.setClothes(6, 153, 0, 0);
-               player.setClothes(3, 1, 0, 0);
+               player.setClothes(0, 0, 0, 2);
+               player.setClothes(1, 0, 0, 2);
+               player.setClothes(3, 14, 0, 2);
+               player.setClothes(4, 34, 0, 2);
+               player.setClothes(5, 0, 0, 2);
+               player.setClothes(6, 25, 0, 2);
+               player.setClothes(7, 0, 0, 2);
+               player.setClothes(8, 35, 0, 2);
+               player.setClothes(9, 0, 0, 2);
+               player.setClothes(10, 0, 0, 2);
+               player.setClothes(11, 48, 0, 2);
             } else { 
-               player.setClothes(11, 142, 0, 0);
-               player.setClothes(8, 120, 0, 0);
-               player.setClothes(4, 10, 0, 0);
-               player.setClothes(6, 153, 0, 0);
-               player.setClothes(7, 2, 0, 0);
+               player.setClothes(0, 0, 0, 2);
+               player.setClothes(1, 0, 0, 2);
+               player.setClothes(3, 0, 0, 2);
+               player.setClothes(4, 35, 0, 2);
+               player.setClothes(5, 0, 0, 2);
+               player.setClothes(6, 25, 0, 2);
+               player.setClothes(7, 0, 0, 2);
+               player.setClothes(8, 58, 0, 2);
+               player.setClothes(9, 0, 0, 2);
+               player.setClothes(10, 0, 0, 2);
+               player.setClothes(11, 55, 0, 2);
             }
 
          }
