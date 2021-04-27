3b36c0845c2db195eaf2a75b5204fe4c170c378b
Marko <marko.koprivanac.19@singimail.rs>
2021-04-28 01:30:28 +0200

    garage

--- packages/focus_roleplay/classes/Garages.js
+++ packages/focus_roleplay/classes/Garages.js
@@ -1,12 +1,28 @@
 mp.garages = {};
 
-class Garages {
+class Garage {
    constructor(id, data) {
       this.id = id;
       this.owner = data.owner || -1;
       this.house = data.house || -1;
       this.type = data.type || 0;
+      this.vehicles = data.vehicles || []
 
       mp.garages[this.id] = this;
    }
 }
+
+class Garages {
+   create = (type) => {
+      db.query("INSERT INTO `garages` (type) VALUES (?)", 
+         [type], function (error, results, fields) {
+         if (error) return core.terminal(1, 'Garage Creating Error ' + error);
+      });
+   }
+
+   delete = (id) => {
+
+   }
+}
+
+   
