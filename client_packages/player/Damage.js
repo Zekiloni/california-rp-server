a0cd9089b99cd26706988e47295221234b9153d6
Marko <marko.koprivanac.19@singimail.rs>
2021-06-25 13:51:06 +0200

    ped

--- client_packages/player/Damage.js
+++ client_packages/player/Damage.js
@@ -5,7 +5,7 @@ var headshots = 0;
 var headshotsTimer = null;
 const player = mp.players.local;
 
-mp.events.add('render',  function() {
+mp.events.add('render', () => {
   let healthLoss = 0, armourLoss = 0;
   if (lastHealth != player.getHealth()) {
     healthLoss = lastHealth - player.getHealth();
@@ -23,16 +23,17 @@ mp.events.add('render',  function() {
 });
 
 mp.events.add('outgoingDamage', (sourceEntity, targetEntity, sourcePlayer, weapon, boneIndex, damage) => {
-  /* Anti Aim 
-  if (targetEntity.type === 'player' && boneIndex === 20) {
+  
+  //Anti Aim 
+  /* if (targetEntity.type === 'player' && boneIndex === targetEntity.getBoneIndexByName('SKEL_Head')) {
       headshots++;
       if(headshotsTimer != null) {
         clearTimeout(myVar);
-        headshotsTimer = setTimeout(() => { headshots = 0; }, 120000);
+        headshotsTimer = setTimeout(() => { headshots = 0; headshotsTimer = null; }, 12000);
       }
       if(headshots > 3) {
         mp.events.callRemote("server:anticheat", 1, player.name);
       }
-  }*/
-  mp.gui.chat.push(`sourceEntity: ${JSON.stringify(sourceEntity)} | targetEntity: ${JSON.stringify(targetEntity)} | sourcePlayer: ${JSON.stringify(sourcePlayer)} | weapon: ${weapon}, boneIndex: ${boneIndex}, damage: ${damage}`)
+  } */
+  //mp.gui.chat.push(`sourceEntity: ${JSON.stringify(sourceEntity)} | targetEntity: ${JSON.stringify(targetEntity)} | sourcePlayer: ${JSON.stringify(sourcePlayer)} | weapon: ${weapon}, boneIndex: ${boneIndex}, damage: ${damage}`)
 });
