fb4319d399d86ab1e5dccb7070a67272e8fc3d6b
Marko Koprivanac <73706747+Kopra99@users.noreply.github.com>
2021-07-04 14:31:32 +0200

    ac

--- client_packages/Anticheat.js
+++ client_packages/Anticheat.js
@@ -12,28 +12,58 @@ const FlaggedWords = ['Cit', 'Čit', 'Admin'],
 let AnticheatSafe = false,
     AnticheatSafeTimer = null,
     Positions = [],
-    Waypoint = null;
+    Waypoint = null,
+    CurrentWeapon = null,
+    CurrentAmmo = 0,
+    CurrentClipSize = 0;
     
 
 
-// Main timer
+/* Main timer
  setInterval(() => {
-   if (Player.admin) return;
+   
    Player.spawned ? AnticheatSafe = false : AnticheatSafe = true; 
-   //if (!Player.spawned) { AnticheatSafe = true; } else { AnticheatSafe = false; }
 
+   // Enabled checks for admins
    SpeedHack ();
    FlyHack ();
    UnAllowedWeapons ();
- }, 1000);
+   
+   // Disable checks for admins
+   if (Player.admin) return;
 
-/*
-// Teleport hack timer
+ }, 1000);*/
+
+
+/* Teleport hack timer
 setInterval(() => {
    TeleportHack ();
-}, 1000);
+}, 1000);*/
 
 
+
+
+/*   
+   0 : 'Health hack',
+   1 : 'Armour hack',
+   2 : 'Godmode',
+   3 : 'Teleport hack',
+   4 : 'Ammo hack',
+   5 : 'Blacklisted weapon',
+   6 : 'High ping',
+   7 : 'Headshot Aim',
+   8 : 'Aim',
+   9 : 'No Reload',
+   10 : 'Speed hack (OnFoot)',
+   11 : 'Speed hack (Vehicle)',
+   12 : 'Vehicle Repair',
+   13 : 'Vehicle Mods',
+   14 : 'Fly/Teleport Hack (OnFoot)',
+   15 : 'Fly/Teleport Hack (Vehicle)',
+   16 : 'Fly/Teleport Hack (Water)',
+   17 : 'Fly/Teleport Hack (Waypoint)' */
+
+/*
 function TeleportHack() {
    Positions.push(Player.position);
    if (Positions.length === 2) {
@@ -43,15 +73,15 @@ function TeleportHack() {
       const Distance = utils.Distance(Vector1, Vector2);
       if (Player.vehicle) {
          if (Distance > 55) {
-            Waypoint === null ? mp.events.callRemote('server:anti_cheat:detected', 16, 'warn') : mp.events.callRemote('server:anti_cheat:detected', 17, 'warn', 'WP');
+            Waypoint === null ? mp.events.callRemote('server:ac.detected', 16, 'warn') : mp.events.callRemote('server:ac.detected', 17, 'warn', 'WP');
          }
       } else if (!Player.vehicle) { 
          if (Distance > 7 && !Player.isFalling()) {
-            Waypoint === null ? mp.events.callRemote('server:anti_cheat:detected', 15, 'warn') : mp.events.callRemote('server:anti_cheat:detected', 15, 'warn', 'WP');
+            Waypoint === null ? mp.events.callRemote('server:ac.detected', 15, 'warn') : mp.events.callRemote('server:ac.detected', 15, 'warn', 'WP');
          }
       } else if (Player.isInWater()) {
          if (Distance > 5) {
-            Waypoint === null ? mp.events.callRemote('server:anti_cheat:detected', 17, 'warn') : mp.events.callRemote('server:anti_cheat:detected', 15, 'warn', 'WP');
+            Waypoint === null ? mp.events.callRemote('server:ac:.etected', 17, 'warn') : mp.events.callRemote('server:ac.detected', 15, 'warn', 'WP');
          }
       }
       Positions = [];
@@ -59,11 +89,18 @@ function TeleportHack() {
 }
 */
 
+function NoReload () {
+   if (AnticheatSafe) return;
+   if (!Player.weapon) return;
+
+
+}
+
 function UnAllowedWeapons () {
    if (AnticheatSafe) return;
    for (const WeaponHash in BlacklistedWeapons) {
       if (Player.Weapon === WeaponHash) {
-        // mp.events.callRemote('server:anti_cheat:detected', 6, 'ban');
+         mp.events.callRemote('server:ac.detected', 6, 'ban'); // Blacklisted weapons
       }
    }
 }
@@ -73,19 +110,12 @@ function SubtractVector(v1, v2) {
 }
 
 function FlyHack () {
-   /*
    if (AnticheatSafe) return;
-   if (Player.isInAir()) {
-      if (!Player.isInAnyHeli() && !Player.isInAnyPlane() && !Player.isRagdoll() && !Player.isFalling()) {
-         mp.events.callRemote('server:anti_cheat:detected', 4, 'warn');
-      } 
-   }*/
 
    const GroundZ = mp.game.gameplay.getGroundZFor3dCoord(Player.position.x, Player.position.y, Player.position.z, parseFloat(0), false);
    if (Player.position.z > GroundZ + 5) {
       if (!Player.isInAnyHeli() && !Player.isInAnyPlane() && !Player.isRagdoll() && !Player.isFalling()) {
-         // mp.events.callRemote('server:anti_cheat:detected', 4, 'warn');
-         // mp.gui.chat.push(`Ground z: ${GroundZ}`);
+         mp.events.callRemote('server:ac.detected', 4, 'warn'); // Flyhack
       }
    }
 }
@@ -103,26 +133,16 @@ function SpeedHack () {
       const MaxSpeed = mp.game.vehicle.getVehicleModelMaxSpeed(Vehicle.model);
       
       if (VehSpeed > MaxSpeed + 10) {
-        // mp.events.callRemote('server:ac.detected', 12, 'warn'); // 6 Warnova kick/ban
+         mp.events.callRemote('server:ac.detected', 12, 'warn'); // Vehicle speed hack
       }
    } 
    else {
       const PedSpeed = Player.getSpeed();
       if (PedSpeed > 6.2) {
-        // mp.events.callRemote('server:ac.detected', 11, 'warn');
+        mp.events.callRemote('server:ac.detected', 11, 'warn'); // On Foot Speedhack
       }
    }
 }
-
-/*function PlayerHealthArmour () {
-   if (AnticheatSafe) return;
-   if (Player.getHealth() != Player.realHealth) {
-
-   } 
-   if (Player.getArmour() != Player.realArmour) {
-
-   } 
-}*/
 
 
 // Dodati Player.frozen sharovanu varijablu zbog anti unfreeze cheate
@@ -164,9 +184,6 @@ mp.events.addDataHandler({
                }, 1000);
             }
          }
-         else if (newValue === 'admin') {
-            AnticheatSafe = true;
-         }
       }
    }
 });
@@ -176,7 +193,7 @@ mp.events.add({
    'playerChat': (text) => {
       for (const i of FlaggedWords) {
          if (text.toLowerCase().includes(i.toLowerCase())) {
-           // mp.events.callRemote('server:ac.chat', text);
+           // mp.events.callRemote('server:ac.chat', text); Forbidden words
          }
       }
    },
@@ -201,9 +218,14 @@ mp.events.add({
          setTimeout(() => {
             const NewHealth = targetEntity.getHealth();
             if (NewHealth != (LastHealth - damage)) {
-               mp.events.callRemote('server:ac:detected', 7, 'Warn');
+               mp.events.callRemote('server:ac:detected', 7, 'Warn'); // Health hack
             }
          }, 250);
       }
+   },
+   'playerWeaponChange': (oldWeapon, newWeapon) => {
+       CurrentWeapon = mp.game.invoke(`0x0A6DB4965674D243`, Player.handle); //GET_SELECTED_PED_WEAPON
+       CurrentAmmo = CurrentWeapon.getWeaponAmmo;
+       CurrentClipSize = mp.game.weapon.getWeaponClipSize(weaponHash);
    }
 });
