a9f6280cb71e11fcd0c54df58bd973a93bccfd3b
Marko <marko.koprivanac.19@singimail.rs>
2021-07-22 22:58:30 +0200

    ts

--- Server/src/Models/Ban.ts
+++ Server/src/Models/Ban.ts
@@ -55,10 +55,10 @@ class Ban extends Model {
    BanPlayer = async function (player: PlayerMp, target: PlayerMp, reason: string, date: Date, expiring: Date, isIp: boolean = false) {
 
       if (isIp) {
-         let IP = Main.ValidateIP(target.ip);
+         const IP = Main.ValidateIP(target.ip);
          if (IP) {
-            const UserAcc = await Account.findOne({ where: { id: player.character } })
-            const Banned = await Ban.create({ IP: IP, Reason: reason, Date: date, Expiring: expiring, Issuer: player.account });
+            const UserAcc = await Account.findOne({ where: { id: player.CHARACTER_ID } })
+            const Banned = await Ban.create({ IP: target.ip, Reason: reason, Date: date, Expiring: expiring, Issuer: player.ACCOUNT_ID });
             if (UserAcc) {
                Banned.Account = UserAcc.id;
                Banned.HardwareId = UserAcc.Hardwer;
@@ -70,23 +70,22 @@ class Ban extends Model {
       else {
          let Online = mp.players.find(target);
          if (Online) {
-            const ActiveChar = await Character.findOne({ where: { id: Online.character } });
+            const ActiveChar = await Character.findOne({ where: { id: Online.CHARACTER_ID } });
 
-            Ban.create({ Account: ActiveChar.id, Character: ActiveChar.id, IP: ActiveChar.ip, Hardwer: ActiveChar.Hardwer, Social: ActiveChar.Social_Club, Date: date, Expiring: expiring, Issuer: player.account });
+            Ban.create({ Account: ActiveChar.id, Character: ActiveChar.id, IP: ActiveChar.ip, Hardwer: ActiveChar.Hardwer, Social: ActiveChar.Social_Club, Date: date, Expiring: expiring, Issuer: player.ACCOUNT_ID });
             Online.kick(reason);
          } else {
             const OfflineChar = await Character.findOne({ where: { Name: target } });
 
             if (OfflineChar) {
-               Ban.create({ Account: OfflineChar.id, Character: OfflineChar.id, IP: OfflineChar.ip, Hardwer: OfflineChar.Hardwer, Social: OfflineChar.Social_Club, Date: date, Expiring: expiring, Issuer: player.account });
+               Ban.create({ Account: OfflineChar.id, Character: OfflineChar.id, IP: OfflineChar.ip, Hardwer: OfflineChar.Hardwer, Social: OfflineChar.Social_Club, Date: date, Expiring: expiring, Issuer: player.ACCOUNT_ID });
             } else {
                // That user is not found
+               player.Notification(Messages.USER_NOT_FOUND, frp.Globals.Notification.Error, 5);
             }
          }
       }
-      
-
-      
+           
    };
 }
 
