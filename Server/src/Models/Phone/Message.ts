70187ff7eb8f146cb933178c1b2e9fc9f6dc15dd
Marko <marko.koprivanac.19@singimail.rs>
2021-07-23 15:16:48 +0200

    Phone gotov

--- Server/src/Models/Phone/Message.ts
+++ Server/src/Models/Phone/Message.ts
@@ -1,51 +1,50 @@
 import { Table, Column, Model, HasMany, PrimaryKey, AutoIncrement, Unique, Default, BeforeCreate, CreatedAt, UpdatedAt, AllowNull, ForeignKey } from 'sequelize-typescript';
 
 export default class Message extends Model {
-    @Column
-    @PrimaryKey
-    @AutoIncrement
-    ID: number;
- 
-    @Column
-    //@ForeignKey(() => SimCard)
-    @AllowNull(false)
-    Sim_Number_From: number;
- 
-    @Column
-    //@ForeignKey(() => SimCard)
-    @AllowNull(false)
-    Sim_Number_To: number;
- 
-    @Column
-    @AllowNull(false)
-    Message: string;
+  @Column
+  @PrimaryKey
+  @AutoIncrement
+  ID: number;
 
-    @Column
-    @UpdatedAt
-    Created_At: Date;
+  @Column
+  //@ForeignKey(() => SimCard)
+  @AllowNull(false)
+  Sim_Number_From: number;
 
-    @Column
-    @UpdatedAt
-    Updated_At: Date;
- }
+  @Column
+  //@ForeignKey(() => SimCard)
+  @AllowNull(false)
+  Sim_Number_To: number;
 
- /* frp.Messages.Send = function (phone, target, message) { 
-   frp.Messages.new({ Phone: phone, Target: target, message: message });
-};
+  @Column
+  @AllowNull(false)
+  MessageText: string;
 
+  @Column
+  @AllowNull(false)
+  @Default(false)
+  Readed: boolean;
 
+  @Column
+  @UpdatedAt
+  Created_At: Date;
 
+  @Column
+  @UpdatedAt
+  Updated_At: Date;
 
-frp.Messages.prototype.Readed = function () { 
-   this.Readed = true;
-   await this.save();
-};
+  static async Send(NumberFrom: number, NumberTo: number, Text: string) {
+    const NewSMS = Message.create({ Sim_Number_From: NumberFrom, Sim_Number_To: NumberTo, MessageText: Text })
+  }
 
-
+  async Seen () {
+    this.Readed = true;
+    await this.save();
+  }
+}
 
 (async () => {
-   frp.Messages.sync();
-   frp.Contacts.sync();
-})();*/
- 
- 
+  await Message.sync();
+})();
+
+
