0a6fb31d0dfa1f33f489a4f9c2be64508119050c
Marko <marko.koprivanac.19@singimail.rs>
2021-07-24 19:02:01 +0200

    Ajmo server side

--- Server/src/Misc/Department.ts
+++ Server/src/Misc/Department.ts
@@ -1,3 +1,9 @@
+import { Colors } from "../Global/Colors";
+import { Globals } from "../Global/Globals";
+import { Messages } from "../Global/Messages";
+import { Vehicles } from "../Models/Vehicles";
+import { Main } from "../Server/Main";
+import { Settings } from "../Server/Settings";
 
 
 
@@ -13,8 +19,6 @@ const Department = {
    }
 };
 
-module.exports = { Department };
-
 
 // const Route = require('../data/Driving.Route');
 // const Quiz = require('../data/Driving.Quiz');
@@ -22,13 +26,13 @@ module.exports = { Department };
 
 mp.events.add({
    'server:vehicle.department.license:give': async (Player, Category, vehicle) => { 
-      frp.Main.Sleep(4).then(async () => { 
+      Main.Sleep(4).then(async () => { 
          frp.GameObjects.TemporaryVehicles[vehicle].destroy();
 
          const Character = await Player.Character();
          Character.GiveLicense(Category);
 
-         Player.Notification(frp.Globals.messages.SUCCEFULLY_GET_LICENSE + Category + '.', frp.Globals.Notification.Succes, 6);
+         Player.Notification(Messages.SUCCEFULLY_GET_LICENSE + Category + '.', Globals.Notification.Succes, 6);
 
       });
    },
@@ -36,7 +40,7 @@ mp.events.add({
    'server:vehicle.department:menu': async (player) => { 
       const Character = await player.Character();
       const Player = { Money: Character.Money, Licenses: Character.Licenses };
-      const Department = { Quiz: Quiz, Licenses: frp.Settings.Licenses };
+      const Department = { Quiz: Quiz, Licenses: Settings.Licenses };
       player.call('client:vehicle.department:menu', [Player, Department]);
    }
 })
@@ -44,32 +48,32 @@ mp.events.add({
 mp.events.addProc({
    'server:vehicle.department.driving:start': (player) => { 
       let Model = Department.Vehicle.Models[Math.floor(Math.random() * Department.Vehicle.Models.length)];
-      const Vehicle = frp.Vehicles.CreateTemporary(Model, Department.Vehicle.Position, Department.Vehicle.Rotation, [Department.Vehicle.Color, Department.Vehicle.Color], 'DMV' + frp.Main.GenerateNumber(3));
-      player.SendMessage(frp.Globals.messages.DMV_INSTRUCTOR_GO_VEHICLE, frp.Globals.Colors.white[0]);
+      const Vehicle = Vehicles.CreateTemporary(Model, Department.Vehicle.Position, Department.Vehicle.Rotation, [Department.Vehicle.Color, Department.Vehicle.Color], 'DMV' + Main.GenerateNumber(3));
+      player.SendMessage(Messages.DMV_INSTRUCTOR_GO_VEHICLE, Colors.white[0]);
    
-      return [Vehicle.id, Route];
+      return [Vehicle, Route];
    }
 })
 
 
-class DMV { 
+export class DMV { 
 
-   static Colshape = mp.colshapes.newRectangle(Department.Position.x, Department.Position.y, Department.Position.z, 2.0, 2.0, frp.Settings.default.dimension);
+   static Colshape = mp.colshapes.newRectangle(Department.Position.x, Department.Position.y, Department.Position.z, 2.0, Settings.Default.dimension);
    static Blip = mp.blips.new(530, new mp.Vector3(Department.Position.x, Department.Position.y, 0), {
       name: 'Department of Motor Vehicles',
       scale: 0.85,
       color: 54,
       alpha: 255,
       shortRange: true,
-      dimension: frp.Settings.default.dimension,
+      dimension: Settings.Default.dimension,
    });
 
    static Working () { 
 
       if (mp.world.time.hour < 6 || mp.world.time.hour > 20) { 
-         DMV.Colshape.OnPlayerEnter = (player) => { player.Notification(frp.Globals.messages.DMV_NOT_WORKING, frp.Globals.Notification.Info, 5); };
+         DMV.Colshape.OnPlayerEnter = (player) => { player.Notification(Messages.DMV_NOT_WORKING, Globals.Notification.Info, 5); };
       }  else { 
-         DMV.Colshape.OnPlayerEnter = (player) => { player.Notification(frp.Globals.messages.DMV_USAGE, frp.Globals.Notification.Info, 5); };
+         DMV.Colshape.OnPlayerEnter = (player) => { player.Notification(Messages.DMV_USAGE, Globals.Notification.Info, 5); };
       }
 
       setTimeout(() => { DMV.Working(); }, 30 * 1000);
@@ -77,4 +81,8 @@ class DMV {
    
 };
 
-DMV.Working();
+(async () => {
+
+   DMV.Working();
+
+})();
