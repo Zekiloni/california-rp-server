3e334b15897e17f97fb5538533d8ae4dbdd382fd
Marko <marko.koprivanac.19@singimail.rs>
2021-07-25 02:49:03 +0200

    push

--- Client/src/Utils.ts
+++ Client/src/Utils.ts
@@ -1,97 +1,86 @@
 
 const Player = mp.players.local;
 
+
 const Server = {
    Color: {
       R: 104, G: 69, B: 234, A: 255
    }
 }
 
-function CompareVectors (i, x) { 
+function CompareVectors (i: Vector3Mp, x: Vector3Mp) { 
    return i.x == x.x && i.y == x.y && i.z == x.z;
 };
 
+function DistanceBetweenVectors (First: Vector3Mp, Second: Vector3Mp) {
+   return new mp.Vector3(First.x, First.y, First.z).subtract(new mp.Vector3(Second.x, Second.y, Second.z)).length();
+}
 
-function LoadAnimDict (i) { 
-   if (mp.game.streaming.hasAnimDictLoaded(i)) return Promise.resolve();
+function LoadAnimationDictionary (i: string): Promise<boolean> { 
+   if (mp.game.streaming.hasAnimDictLoaded(i)) return Promise.resolve(true);
    return new Promise(async resolve => { 
       mp.game.streaming.requestAnimDict(i);
       while (!mp.game.streaming.hasAnimDictLoaded(i)) { 
-         await mp.game.waitAsync(0);bro
+         await mp.game.waitAsync(0);
       }
-      resolve();
+      resolve(true);
    })
 };
 
 
-function LoadMovementClipset (Clipset) { 
-   if (mp.game.streaming.hasClipSetLoaded(Clipset)) return Promise.resolve();
+function LoadMovementClipset (Clipset: string): Promise<boolean> { 
+   if (mp.game.streaming.hasClipSetLoaded(Clipset)) return Promise.resolve(true);
    return new Promise(async resolve => { 
       mp.game.streaming.requestClipSet(Clipset);
       while (!mp.game.streaming.hasClipSetLoaded(Clipset)) { 
          await mp.game.waitAsync(10);
       }
-      resolve();
+      resolve(true);
    })
 }
 
 
 
-function WaitEntity (entity) {
+function WaitEntity (Entity: EntityMp) {
    return new Promise(resolve => {
       let wait = setInterval(() => {
-         if (mp.game.entity.isAnEntity(entity.handle)) {
+         if (mp.game.entity.isAnEntity(Entity.handle)) {
             clearInterval(wait);
-            resolve();
+            resolve(true);
          }
       }, 1);
    });
 }
 
-function weaponString (weapon) {
-	if (typeof weapon !== 'undefined')
-		return '0x' + weapon.toString(16).toUpperCase()
+function WeaponString (Weapon: number) {
+	if (typeof Weapon !== 'undefined')
+		return '0x' + Weapon.toString(16).toUpperCase()
 	else 
 		return '0xA2719263'
 }
 
 
 
-function Distance (first, second) {
-   return new mp.Vector3(first.x, first.y, first.z).subtract(new mp.Vector3(second.x, second.y, second.z)).length();
-}
-
-
-function OnlinePlayers () {
-   let list = [];
+export function OnlinePlayers () {
+   let List: any = [];
    mp.players.forEach(_Player => {
-      list.push({ id: _Player.remoteId, name: _Player.name }); 
+      List.push({ id: _Player.remoteId, name: _Player.name }); 
    }); 
-   return list;
+   return List;
 }
 
 
-function GetAdress (position) { 
-   const path = mp.game.pathfind.getStreetNameAtCoord(position.x, position.y, position.z, 0, 0),
-      Zone = mp.game.gxt.get(mp.game.zone.getNameOfZone(position.x, position.y, position.z)),
+export function GetAdress (Position: Vector3Mp) { 
+   const path = mp.game.pathfind.getStreetNameAtCoord(Position.x, Position.y, Position.z, 0, 0),
+      Zone = mp.game.gxt.get(mp.game.zone.getNameOfZone(Position.x, Position.y, Position.z)),
       Street = mp.game.ui.getStreetNameFromHashKey(path.streetName);
    return { zone: Zone, street: Street };
 }
 
+let MovableCamera: CameraMp;
 
-function BrowserControls (freezeControls, mouse) {
-   mouse ? mp.gui.chat.activate(false) : mp.gui.chat.activate(true);
-   
-   setTimeout(() => { mp.gui.cursor.show(freezeControls, mouse); }, 250);
-}
-
-Player.BrowserControls = BrowserControls;
-
-
-let MovableCamera = null;
-
-function PlayerPreviewCamera (toggle) { 
-   if (toggle) { 
+export function PlayerPreviewCamera (Toggle: boolean) { 
+   if (Toggle) { 
       MovableCamera = mp.cameras.new('default', new mp.Vector3(0, 0, 0), new mp.Vector3(0, 0, 0), 40);
       const CameraPositon = new mp.Vector3(Player.position.x + Player.getForwardX() * 1.5, Player.position.y + Player.getForwardY() * 1.5, Player.position.z);
       MovableCamera.setCoord(CameraPositon.x, CameraPositon.y, CameraPositon.z);
@@ -105,15 +94,14 @@ function PlayerPreviewCamera (toggle) {
       mp.events.remove('render', MoveCamera);
       mp.events.remove('client:player.camera:zoom', ZoomCamera);
       if (MovableCamera) MovableCamera.destroy();
-      MovableCamera = null;
       mp.game.cam.renderScriptCams(false, false, 0, false, false);
    }
 }
 
-function ZoomCamera (delta) {
+function ZoomCamera (Delta: number) {
    let { x, y, z } = MovableCamera.getCoord();
 
-   if (delta < 0) { 
+   if (Delta < 0) { 
       x += MovableCamera.getDirection().x * 0.1;
       y += MovableCamera.getDirection().y * 0.1;
       
@@ -130,14 +118,14 @@ function ZoomCamera (delta) {
 
 let [PrevX, PrevY] = mp.gui.cursor.position;
 
-function CursorData () { 
+export function CursorData () { 
    const x = PrevX, y = PrevY;
    PrevX = mp.gui.cursor.position[0];
    PrevY = mp.gui.cursor.position[1];
    return { DeltaX: mp.gui.cursor.position[0] - x, DeltaY: mp.gui.cursor.position[1] - y };
 }
 
-function MoveCamera () { 
+export function MoveCamera () { 
    const Data = CursorData();
 
    if (!mp.keys.isDown(0x02)) return;
@@ -153,17 +141,15 @@ function MoveCamera () {
    if (camPosZ < charPosZ + 0.7 && camPosZ > charPosZ - 0.8) { 
       MovableCamera.setPosition(camPosX, camPosY, camPosZ);
       MovableCamera.pointAtCoord(charPosX, charPosY, camPosZ);
+
+      
    }
 }
 
 
-function CreateInteractionSpot (name, position) { 
-   const checkpoint = mp.checkpoints.new(48, position, 2.5, { color: [196, 12, 28, 195], visible: true, dimension: Player.dimension });
-   const blip = mp.blips.new(1, new mp.Vector3(position.x, position.y, 0), { name: name, color: 1, shortRange: false });
+export function CreateInteractionSpot (Name: string, Position: Vector3Mp) { 
+   const checkpoint = mp.checkpoints.new(48, Position, 2.5, { color: [196, 12, 28, 195], visible: true, dimension: Player.dimension });
+   const blip = mp.blips.new(1, new mp.Vector3(Position.x, Position.y, 0), { name: Name, color: 1, shortRange: false });
    return { checkpoint: checkpoint, blip: blip };
 };
 
-Player.CreateInteractionSpot = CreateInteractionSpot;
-
-
-global.utils = { CompareVectors, LoadAnimDict, weaponString, Distance, OnlinePlayers, GetAdress, PlayerPreviewCamera, WaitEntity, LoadMovementClipset, Server };
