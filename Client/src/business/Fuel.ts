72b97f802463cb9f3e6511fb44451b8b39d3df20
Marko <marko.koprivanac.19@singimail.rs>
2021-07-27 13:49:56 +0200

    CS TS

--- Client/src/business/Fuel.ts
+++ Client/src/business/Fuel.ts
@@ -1,25 +1,24 @@
-
-
+export {};
 
 const Player = mp.players.local;
 let BusinessGasMenu: boolean = false;
 
-const Pumps = [ 1339433404, 1694452750, 1933174915, 2287735495 ];
+const Pumps = [1339433404, 1694452750, 1933174915, 2287735495];
 
 
 mp.events.add({
-   'client:business.gas:menu': (Info: any) => { 
+   'client:business.gas:menu': (Info: any) => {
       BusinessGasMenu = !BusinessGasMenu;
-      
+
    }
 })
 
 
 mp.events.addProc({
-   'client:business.gas:nearpump': async () => { 
+   'client:business.gas:nearpump': (): Promise<boolean> => {
       const Position = Player.position;
-      return new Promise((resolve) => { 
-         for (const Pump of Pumps) { 
+      return new Promise((resolve) => {
+         for (const Pump of Pumps) {
             let object = mp.game.object.getClosestObjectOfType(Position.x, Position.y, Position.z, 3.0, Pump, false, true, true);
             if (object) {
                resolve(true)
@@ -29,3 +28,5 @@ mp.events.addProc({
       });
    }
 });
+
+
