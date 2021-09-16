957eb9d949d77e760261acac2f28e197beec90c9
Marko Koprivanac <73706747+Kopra99@users.noreply.github.com>
2021-09-16 17:59:40 +0200

    Delete Server/dist directory
    brisanje dist server side

--- Server/dist/Global/Commands.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Commands = void 0;
-exports.Commands = {
-    Lock: 'lock',
-    Trunk: 'trunk',
-    Hood: 'hood'
-};
--- Server/dist/models/Injury.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HumanBones = void 0;
-var HumanBones;
-(function (HumanBones) {
-})(HumanBones = exports.HumanBones || (exports.HumanBones = {}));
