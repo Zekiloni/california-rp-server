a9c8dbe5fe24634eaf47ef6fa8a724a3d1abcce5
Zekiloni <zekirija17@gmail.com>
2022-05-27 00:11:13 +0200

    knjazevacka 73

---
+++ browser/src/components/items/phone/WeatherApp.vue
@@ -0,0 +1,16 @@
+
+<template>
+   <div class="">
+      
+   </div>
+</template>
+
+<script lang="ts">
+   import Vue from 'vue'
+   import Component from 'vue-class-component';
+
+   @Component
+   export default class WeatherApp extends Vue {
+
+   }
+</script>
